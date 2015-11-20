9daf2622c2c43547020f3a1e17d1e87891b85972
Aditya <addiittya2006@gmail.com>
2015-11-20 22:23:54 +0530

    remove junk

--- Py/dict_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python3
-
-def main():
-	newdict = dict(adi = 'son', prag='daughter', rachna='mother', neeraj='father')
-	ent = 'adi'
-	print(newdict[ent])
-
-if __name__ == '__main__':
-	main()
